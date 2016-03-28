ebfaea0074889d9e2627766036d2f4848b8b8082
Wilbur Donovan <wilbur.donovan91@hotmail.com>
2016-03-28 21:54:55 +1000

    Add second display, add functionality for more buttons, minor styling adjustments, minor bug fixes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # javascriptCalculator
+Bugs known for functionality already implemented:
+    - Pressing the "Del" button when primary display is
+    a length of 1 will get rid of the div height for the
+    primary display div.
--- functionality.js
+++ functionality.js
@@ -3,34 +3,95 @@
  */
 function main() {
     //Variables
-    var dispValue = document.getElementById("dispValue"), valueToDisplay = "", calcButtons = document.getElementsByClassName("calcButton"), i;
+    var dispValue = document.getElementById("dispValue"),      
+        dispValue2 = document.getElementById("dispValue2"), secondaryDispVal = "", primaryDispVal = "", calcButtons = document.getElementsByClassName("calcButton"), i;
 
     //Basic calculator object
     var calcObject = {
-        currentlyDisplayed: "0",
-        updateDisplay: function () {
-            this.currentlyDisplayed = valueToDisplay;
+        primaryDisplay: "0",
+        secondaryDisplay: "0",
+        updateDisplay: function (m , n) {
+           if (m == null) {
+                this.primaryDisplay = primaryDispVal;
+            } else {
+                this.primaryDisplay = m;
+            }
+            if (n == null) {
+                this.secondaryDisplay = secondaryDispVal;
+            } else {
+                this.secondaryDisplay = n;
+            }  
         }
     };
     
     //Updates the object & the DOM
     function updateDisplay() {
         calcObject.updateDisplay();
-        dispValue.innerHTML = calcObject.currentlyDisplayed;
+        dispValue.innerHTML = calcObject.primaryDisplay;
+        dispValue2.innerHTML = calcObject.secondaryDisplay;
     }
    
+    //Adds the buttons value to the dispValue strings and updates display
     function buttonClick() {
-        valueToDisplay += this.innerHTML;
+        var hold = this.innerHTML;
+        if (hold == "0" || hold == "1" || hold == "2" || hold == "3" || hold == "4" || hold == "5" || hold == "6" || hold == "7" || hold == "8" || hold == "9" || hold == ".") {
+            if (primaryDispVal != "0") {
+                primaryDispVal += hold;
+            } else {
+                primaryDispVal = hold;
+            }
+        } 
+        if (hold == "+" || hold == "-" || hold == "/" || hold == "*") {
+            primaryDispVal = "0";
+            secondaryDispVal += " " + hold + " ";
+        } else if (hold == "=") {
+            // Calculate answers here
+            
+            secondaryDispVal += " =";
+        } else {
+            secondaryDispVal += hold;
+        }
+        
         updateDisplay();
     }
     
+    //Clears screen when CE is clicked
+    function CEClick() {
+        secondaryDispVal = "";
+        primaryDispVal = "0";
+        calcObject.updateDisplay("0", " ");
+        updateDisplay();
+    }
+    
+    //Deletes last character in display
+    function delClick() {
+        primaryDispVal = primaryDispVal.substring(0, primaryDispVal.length-1);
+        secondaryDispVal = secondaryDispVal.substring(0, secondaryDispVal.length-1);
+        updateDisplay();
+    }
+    
+    //Clears primary display when C is clicked
+    function CClick () {
+        secondaryDispVal = secondaryDispVal.substring(0, secondaryDispVal.length-primaryDispVal.length)
+        primaryDispVal = "0";
+        updateDisplay();
+    }
     
     //Initial Dom Update
-    dispValue.innerHTML = calcObject.currentlyDisplayed;
+    dispValue.innerHTML = calcObject.primaryDisplay;
     
     //Add event listeners to the buttons
     for (i = 0; i < calcButtons.length; i++) {
-        calcButtons[i].addEventListener("click", buttonClick);
+        if (calcButtons[i].innerHTML == "CE") {
+            calcButtons[i].addEventListener("click", CEClick);
+        } else if (calcButtons[i].innerHTML == "Del") {
+            calcButtons[i].addEventListener("click", delClick);
+        } else if (calcButtons[i].innerHTML == "C") {
+            calcButtons[i].addEventListener("click", CClick);
+        }
+        else {
+            calcButtons[i].addEventListener("click", buttonClick);
+        }
     }
 }
 
